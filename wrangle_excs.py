e62154e946ede9e02b00088d7f31c48cd0b4d2db
Zach Gulde <zachgulde@gmail.com>
2021-09-20 13:25:16 -0500

    Add explore lesson notes

--- wrangle_excs.py
+++ wrangle_excs.py
@@ -10,32 +10,32 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 sql = """
-SELECT prop.*, 
-       pred.logerror, 
-       pred.transactiondate, 
-       air.airconditioningdesc, 
-       arch.architecturalstyledesc, 
-       build.buildingclassdesc, 
-       heat.heatingorsystemdesc, 
-       landuse.propertylandusedesc, 
-       story.storydesc, 
-       construct.typeconstructiondesc 
-FROM   properties_2017 prop  
+SELECT prop.*,
+       pred.logerror,
+       pred.transactiondate,
+       air.airconditioningdesc,
+       arch.architecturalstyledesc,
+       build.buildingclassdesc,
+       heat.heatingorsystemdesc,
+       landuse.propertylandusedesc,
+       story.storydesc,
+       construct.typeconstructiondesc
+FROM   properties_2017 prop
        INNER JOIN (SELECT parcelid,
-                   Max(transactiondate) transactiondate 
-                   FROM   predictions_2017 
-  
+                   Max(transactiondate) transactiondate
+                   FROM   predictions_2017
+
                    GROUP  BY parcelid) pred
                USING (parcelid)
                			JOIN predictions_2017 as pred USING (parcelid, transactiondate)
-       LEFT JOIN airconditioningtype air USING (airconditioningtypeid) 
-       LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid) 
-       LEFT JOIN buildingclasstype build USING (buildingclasstypeid) 
-       LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid) 
-       LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid) 
-       LEFT JOIN storytype story USING (storytypeid) 
-       LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid) 
-WHERE  prop.latitude IS NOT NULL 
+       LEFT JOIN airconditioningtype air USING (airconditioningtypeid)
+       LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid)
+       LEFT JOIN buildingclasstype build USING (buildingclasstypeid)
+       LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid)
+       LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid)
+       LEFT JOIN storytype story USING (storytypeid)
+       LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid)
+WHERE  prop.latitude IS NOT NULL
        AND prop.longitude IS NOT NULL
 """
 
@@ -62,7 +62,7 @@ def handle_missing_values(df, prop_required_column = .5, prop_required_row = .70
     return df
 
 
-def remove_columns(df, cols_to_remove):  
+def remove_columns(df, cols_to_remove):
 	#remove columns not needed
     df = df.drop(columns=cols_to_remove)
     return df
@@ -73,24 +73,24 @@ def wrangle_zillow():
         df.to_csv('zillow_cached.csv',index = False)
     else:
         df = pd.read_csv('zillow_cached.csv')
-    
+
     # Restrict df to only properties that meet single use criteria
     single_use = [261, 262, 263, 264, 266, 268, 273, 276, 279]
     df = df[df.propertylandusetypeid.isin(single_use)]
-    
+
     # Restrict df to only those properties with at least 1 bath & bed and 350 sqft area
     df = df[(df.bedroomcnt > 0) & (df.bathroomcnt > 0) & ((df.unitcnt<=1)|df.unitcnt.isnull())\
             & (df.calculatedfinishedsquarefeet>350)]
 
     # Handle missing values i.e. drop columns and rows based on a threshold
     df = handle_missing_values(df)
-    
+
     # Add column for counties
     df['county'] = df['fips'].apply(
         lambda x: 'Los Angeles' if x == 6037\
         else 'Orange' if x == 6059\
         else 'Ventura')
-    
+
     # drop unnecessary columns
     dropcols = ['parcelid',
          'calculatedbathnbr',
@@ -104,13 +104,13 @@ def wrangle_zillow():
          'propertylandusedesc']
 
     df = remove_columns(df, dropcols)
-    
+
     # replace nulls in unitcnt with 1
     df.unitcnt.fillna(1, inplace = True)
-    
+
     # assume that since this is Southern CA, null means 'None' for heating system
     df.heatingorsystemdesc.fillna('None', inplace = True)
-    
+
     # replace nulls with median values for select columns
     df.lotsizesquarefeet.fillna(7313, inplace = True)
     df.buildingqualitytypeid.fillna(6.0, inplace = True)
@@ -118,10 +118,10 @@ def wrangle_zillow():
     # Columns to look for outliers
     df = df[df.taxvaluedollarcnt < 5_000_000]
     df = df[df.calculatedfinishedsquarefeet < 8000]
-    
+
     # Just to be sure we caught all nulls, drop them here
     df = df.dropna()
-    
+
     return df
 
 def min_max_scaler(train, valid, test):
@@ -144,35 +144,40 @@ def outlier_function(df, cols, k):
         q3 = df.annual_income.quantile(0.75)
         iqr = q3 - q1
         upper_bound =  q3 + k * iqr
-        lower_bound =  q1 - k * iqr     
+        lower_bound =  q1 - k * iqr
         df = df[(df[col] < upper_bound) & (df[col] > lower_bound)]
     return df
 
 def get_mall_customers(sql):
-	    url = get_db_url('mall_customers')
-	    mall_df = pd.read_sql(sql, url, index_col='customer_id')
-	    return mall_df
+    url = get_db_url('mall_customers')
+    mall_df = pd.read_sql(sql, url, index_col='customer_id')
+    return mall_df
+
+
+def train_validate_test_split(df):
+    train_and_validate, test = train_test_split(df, train_size=0.8, random_state=123)
+    train, validate = train_test_split(train_and_validate, train_size=0.75, random_state=123)
+    return train, validate, test
+
 
 def wrangle_mall_df():
-    
+
     # acquire data
     sql = 'select * from customers'
 
 
     # acquire data from SQL server
     mall_df = get_mall_customers(sql)
-    
+
     # handle outliers
     mall_df = outlier_function(mall_df, ['age', 'spending_score', 'annual_income'], 1.5)
-    
+
     # get dummy for gender column
     dummy_df = pd.get_dummies(mall_df.gender, drop_first=True)
     mall_df = pd.concat([mall_df, dummy_df], axis=1).drop(columns = ['gender'])
 
-    # split the data in train, validate and test
-    train, test = train_test_split(mall_df, train_size = 0.8, random_state = 123)
-    train, validate = train_test_split(train, train_size = 0.75, random_state = 123)
-    
+    train, validate, test = train_validate_test_split(mall_df)
+
     return min_max_scaler(train, validate, test)
 
 def nulls_by_col(df):
@@ -193,7 +198,7 @@ def nulls_by_row(df):
 
 def summarize(df):
     '''
-    summarize will take in a single argument (a pandas dataframe) 
+    summarize will take in a single argument (a pandas dataframe)
     and output to console various statistics on said dataframe, including:
     # .head()
     # .info()
